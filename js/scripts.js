39942edcc1da44088a357a395200459d50e5b375
polibear21 <71219543+polibear21@users.noreply.github.com>
2021-07-28 15:13:59 -0400

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ window.addEventListener('DOMContentLoaded', event => {
         if (!navbarCollapsible) {
             return;
         }
-        if (window.scrollY === 0) {
+        if (window.scrollY < 590) { //change this to change when the nav bar changes color
             navbarCollapsible.classList.remove('navbar-shrink')
         } else {
             navbarCollapsible.classList.add('navbar-shrink')
