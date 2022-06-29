3194d3acc8f7a08c70e80dd5e648bfbb7ca9ff50
Robotic Reaper <liubaoren2006@gmail.com>
2022-06-28 20:43:42 -0400

    test

--- about-us/about-us.js
+++ about-us/about-us.js
@@ -4,22 +4,9 @@ function renderPeople() {
       // founding member section
       jsonResponse.founding = jsonResponse.founding.filter(person => person.firstName + " " + person.lastName !== "Wei Ding");
 
-      $.get("about-us/about-us.hbs", function(template) {
-        // Use the handlebars template to generate the HTML
-        // from people's information.
-        var peopleTemplate = Handlebars.compile(template);
-        $("#founding").append(peopleTemplate(jsonResponse));
-      })
 
       // leader section
       jsonResponse.leader = jsonResponse.leader.filter(person => person.firstName + " " + person.lastName !== "Wei Ding");
-
-      $.get("about-us/about-us.hbs", function(template) {
-        // Use the handlebars template to generate the HTML
-        // from people's information.
-        var peopleTemplate = Handlebars.compile(template);
-        $("#leader").append(peopleTemplate(jsonResponse));
-      })
 
       // team member section
       jsonResponse.member = jsonResponse.member.filter(person => person.firstName + " " + person.lastName !== "Wei Ding");
