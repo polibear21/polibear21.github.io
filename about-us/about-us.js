7e9dbf1d9a722baf209927bee4621dc184fe2459
Robotic Reaper <liubaoren2006@gmail.com>
2022-06-28 20:30:53 -0400

    changed the way people are listed

--- about-us/about-us.js
+++ about-us/about-us.js
@@ -1,18 +1,34 @@
 function renderPeople() {
     $.getJSON("about-us/Members.json", function(jsonResponse) {
-      // Sort the people by last name.
-      // Commented out to display the people by original order
-      // jsonResponse.people.sort(function(a,b) {
-      //   return a.lastName.localeCompare(b.lastName);
-      // });
-  
-      jsonResponse.people = jsonResponse.people.filter(person => person.firstName + " " + person.lastName !== "Wei Ding");
+
+      // founding member section
+      jsonResponse.founding = jsonResponse.founding.filter(person => person.firstName + " " + person.lastName !== "Wei Ding");
 
       $.get("about-us/about-us.hbs", function(template) {
         // Use the handlebars template to generate the HTML
         // from people's information.
         var peopleTemplate = Handlebars.compile(template);
-        $("#us").append(peopleTemplate(jsonResponse));
+        $("#founding").append(peopleTemplate(jsonResponse));
+      })
+
+      // leader section
+      jsonResponse.leader = jsonResponse.leader.filter(person => person.firstName + " " + person.lastName !== "Wei Ding");
+
+      $.get("about-us/about-us.hbs", function(template) {
+        // Use the handlebars template to generate the HTML
+        // from people's information.
+        var peopleTemplate = Handlebars.compile(template);
+        $("#leader").append(peopleTemplate(jsonResponse));
+      })
+
+      // team member section
+      jsonResponse.member = jsonResponse.member.filter(person => person.firstName + " " + person.lastName !== "Wei Ding");
+
+      $.get("about-us/about-us.hbs", function(template) {
+        // Use the handlebars template to generate the HTML
+        // from people's information.
+        var peopleTemplate = Handlebars.compile(template);
+        $("#member").append(peopleTemplate(jsonResponse));
       })
     })
   }
