ee5568fa18f5bfe2f6b70916bc163a8faa0e6fc0
BDimension7 <54754524+BDimension7@users.noreply.github.com>
2022-07-05 22:56:50 -0400

    Updated navbar
    Navbar now only collapses for links on current page (e.g. "#Mission").
    Use the class ".on-page" to indicate a link points to the current file.

--- js/scripts.js
+++ js/scripts.js
@@ -8,18 +8,18 @@
 //
 
 function dropdown() {
-    document.getElementById("myDropdown").classList.add("show");
+    document.getElementById('myDropdown').classList.add('show');
 }
 
 // Close the dropdown if the user clicks outside of it
-window.addEventListener("click", (event) => {
-    if (!event.target.matches(".dropbtn")) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
+window.addEventListener('click', (event) => {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName('dropdown-content');
         var i;
         for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains("show")) {
-                openDropdown.classList.remove("show");
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
             }
         }
     }
@@ -29,25 +29,25 @@ window.homepagecheck = function () {
     // if we are on the homepage aka index.html
     var check = false;
 
-    if (window.location.pathname.includes("index.html")) {
+    if (window.location.pathname.includes('index.html')) {
         // the pathname should be changed
         check = true;
     }
     return check;
 };
 
-window.addEventListener("DOMContentLoaded", (event) => {
+window.addEventListener('DOMContentLoaded', (event) => {
     // Navbar shrink function
     var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector("#mainNav");
+        const navbarCollapsible = document.body.querySelector('#mainNav');
         if (!navbarCollapsible) {
             return;
         }
         if (window.scrollY < 590 && window.homepagecheck()) {
             // change this to change when the nav bar changes color
-            navbarCollapsible.classList.remove("navbar-shrink");
+            navbarCollapsible.classList.remove('navbar-shrink');
         } else {
-            navbarCollapsible.classList.add("navbar-shrink");
+            navbarCollapsible.classList.add('navbar-shrink');
         }
     };
 
@@ -55,25 +55,25 @@ window.addEventListener("DOMContentLoaded", (event) => {
     navbarShrink();
 
     // Shrink the navbar when page is scrolled
-    document.addEventListener("scroll", navbarShrink);
+    document.addEventListener('scroll', navbarShrink);
 
     // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector("#mainNav");
+    const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
-            target: "#mainNav",
+            target: '#mainNav',
             offset: 74,
         });
     }
 
     // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector(".navbar-toggler");
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
-        document.querySelectorAll("#navbarResponsive .nav-link")
+        document.querySelectorAll('#navbarResponsive .on-page')
     );
     responsiveNavItems.map((responsiveNavItem) => {
-        responsiveNavItem.addEventListener("click", () => {
-            if (window.getComputedStyle(navbarToggler).display !== "none") {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
                 navbarToggler.click();
             }
         });
@@ -81,6 +81,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
     // Activate SimpleLightbox plugin for portfolio items
     new SimpleLightbox({
-        elements: "#portfolio a.portfolio-box",
+        elements: '#portfolio a.portfolio-box',
     });
 });
