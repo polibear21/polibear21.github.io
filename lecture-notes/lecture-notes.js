987b61dab01c8072400f8d430f18167625ff5ef5
Kevin Zhu <54754524+BrilliantDeviation7@users.noreply.github.com>
2023-03-28 21:21:32 -0400

    Refactor lecture-notes.js
    Added option to set example name (changed Handlebars file to do so)

--- lecture-notes/lecture-notes.js
+++ lecture-notes/lecture-notes.js
@@ -1,25 +1,23 @@
 function populateRows() {
-  $.getJSON("lecture-notes/lecture-notes.json", function(jsonResponse) {
+  $.getJSON("lecture-notes/lecture-notes.json", (jsonResponse) => {
     // Sort the lecture notes by date
-    jsonResponse.lectureNotes.sort(function(a,b) {
-      return new Date(a.Date) - new Date(b.Date)
+    jsonResponse.lectureNotes.sort((a, b) => {
+      return new Date(a.Date) - new Date(b.Date);
     });
 
-    $.get("lecture-notes/lecture-notes.hbs", function(template) {
+    $.get("lecture-notes/lecture-notes.hbs", (template) => {
       // Use the handlebars template to generate the HTML
       // from the lecture notes.
       var notesTemplate = Handlebars.compile(template);
       $("#lectureNotes tbody").append(notesTemplate(jsonResponse));
-    })
-  })
+    });
+  });
 }
 
-$(document).ready(function () {
-    populateRows();
-})
+$(document).ready(() => {
+  populateRows();
+});
 
-
-Handlebars.registerHelper("inc", function(value, options)
-{
-    return parseInt(value) + 1;
+Handlebars.registerHelper("inc", (value, options) => {
+  return parseInt(value) + 1;
 });
