68fb0a736d48be637f374f1dc0e26feba2131092
Kevin Zhu <54754524+BrilliantDeviation7@users.noreply.github.com>
2023-04-16 12:20:06 -0400

    Completed org chart

--- about-us/OrgChart/OrgChart.js
+++ about-us/OrgChart/OrgChart.js
@@ -1,37 +1,41 @@
 function SetOrgChart() {
-    $.getJSON("about-us/Members.json", (jsonResponse) => {
-        var teams = jsonResponse.teams.map((team) => {
-            var leadHTML = '';
+    $.getJSON("about-us/Members.json", (data) => {
+        const people = data.member.concat(data.leader).concat(data.founding);
+        var teams = data.teams.map((team) => {
+            let leadHTML = '';
             
-            for (var i = 0; i < team.leads.length; i++) {
-                var imgLink = jsonResponse.people.find(person => person.firstName + " " + person.lastName === team.leads[i]).imageLink;
-                leadHTML += '<div class = "leader">' + '<img src="./images/' + imgLink + '" class="avatar"></img>' + '<div>' + team.leads[i] + '</div></div>';
-                
+            for (let i = 0; i < team.leads.length; i++) {
+                let imageLink = people.find((person) => person.firstName + " " + person.lastName === team.leads[i]).imageLink;
+                leadHTML += '<div class = "leader">' + '<img src="./images/' + imageLink + '" class="avatar"></img>' + '<div>' + team.leads[i] + '</div></div>';
             }
             return [{'v':team.name, 'f':'<div class="teamName">' + team.name + '</div>' + 
             '<div class="leaders">' + leadHTML}, "Wei Ding", '']
         })
 
-        var people = [];
-        for (var i = 0; i < jsonResponse.people.length; i++) {
-            var person = jsonResponse.people[i];
-            var fullName = person.firstName + " " + person.lastName;
-            if (!isLead(fullName, jsonResponse.teams)) {
-                var team = jsonResponse.teams.find(team => team.members.includes(fullName));
+        const members = [];
+        for (const person of people) {
+            let fullName = person.firstName + " " + person.lastName;
+            if (!isLead(fullName, data.teams)) {
+                let team = data.teams.find(team => team.members.includes(fullName));
                 if (team === undefined && fullName != "Wei Ding") {
                     team = 'Wei Ding';
                 } else if (fullName === "Wei Ding") {
                     team = '';
-                }
-                else {
+                } else {
                     team = team.name;
                 }
-                people.push([{'v':fullName, 'f': '<img src="./images/' + person.imageLink + '" class="avatar"></img>' + '<div>' + fullName + '</div>'}, team, '']) 
+                members.push([
+                    {
+                        'v': fullName,
+                        'f': '<img src="./images/' + person.imageLink + '" class="avatar"></img>' + '<div>' + fullName + '</div>'
+                    },
+                    team,
+                    '',
+                ]);
             }
         }
-        console.log(people);
 
-        google.charts.load('current', {packages:["orgchart"]});
+        google.charts.load('current', { packages: ["orgchart"] });
         google.charts.setOnLoadCallback(drawChart);
 
         function drawChart() {
@@ -42,22 +46,22 @@ function SetOrgChart() {
 
             // For each orgchart box, provide the name, manager, and tooltip to show.
             data.addRows(teams);
-            data.addRows(people);
+            data.addRows(members);
 
             // Create the chart.
             var chart = new google.visualization.OrgChart(document.getElementById('orgchart'));
             // Draw the chart, setting the allowHtml option to true for the tooltips.
-            chart.draw(data, {'allowHtml':true, size: 'small'});
+            chart.draw(data, { 'allowHtml': true, size: 'small' });
         }
     })
 }
 
 function isLead(name, teams) {
-    for (var i = 0; i < teams.length; i++) {
-        if (teams[i].leads.find(lead => lead === name))
+    for (const team of teams) {
+        if (team.leads.includes(name)) {
             return true;
+        }
     }
-
     return false;
 }
 
