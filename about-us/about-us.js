4e085940ee43d340a86fe28d04dc85bf2e58af41
Kevin Zhu <54754524+BrilliantDeviation7@users.noreply.github.com>
2023-04-16 12:13:39 -0400

    Format about-us.js

--- about-us/about-us.js
+++ about-us/about-us.js
@@ -1,43 +1,35 @@
-
 function renderPeople() {
-    $.getJSON("about-us/Members.json", function(jsonResponse) {
+  $.getJSON("about-us/Members.json", function (jsonResponse) {
+    $.get("about-us/founding.hbs", function (template) {
+      var peopleTemplate = Handlebars.compile(template);
+      $("#founding").append(peopleTemplate(jsonResponse));
+    });
 
-      $.get("about-us/founding.hbs", function(template) {
-        // Use the handlebars template to generate the HTML
-        // from people's information.
-        var peopleTemplate = Handlebars.compile(template);
-        $("#founding").append(peopleTemplate(jsonResponse));
-      })
+    $.get("about-us/leader.hbs", function (template) {
+      var peopleTemplate = Handlebars.compile(template);
+      $("#leader").append(peopleTemplate(jsonResponse));
+    });
 
-      $.get("about-us/leader.hbs", function(template) {
-        var peopleTemplate = Handlebars.compile(template);
-        $("#leader").append(peopleTemplate(jsonResponse));
-      })
+    $.get("about-us/member.hbs", function (template) {
+      var peopleTemplate = Handlebars.compile(template);
+      $("#member").append(peopleTemplate(jsonResponse));
+    });
+  });
+}
 
-      $.get("about-us/member.hbs", function(template) {
-        var peopleTemplate = Handlebars.compile(template);
-        $("#member").append(peopleTemplate(jsonResponse));
-      })
-    })
-  }
-  
-  $(document).ready(function () {
-      renderPeople();
-      
-      $("#memberdirectorybtn").focus();
-      $("#orgchart").hide();
+$(document).ready(function () {
+  renderPeople();
 
-      $("#memberdirectorybtn").click(function() {
-        $("#memberdirectory").show();
-        $("#orgchart").hide();
-      });
+  $("#memberdirectorybtn").focus();
+  $("#orgchart").hide();
 
-      $("#orgchartbtn").click(function() {
-        $("#memberdirectory").hide();
-        $("#orgchart").show();
-      });
- 
-  })
+  $("#memberdirectorybtn").click(function () {
+    $("#memberdirectory").show();
+    $("#orgchart").hide();
+  });
 
-
-  
+  $("#orgchartbtn").click(function () {
+    $("#memberdirectory").hide();
+    $("#orgchart").show();
+  });
+});
